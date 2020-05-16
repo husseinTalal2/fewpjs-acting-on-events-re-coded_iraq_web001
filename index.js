473cf27ced2b6b4aa290966995b66f418dcd63f9
Hussein Talal <hossientalal2@gmail.com>
2020-05-16 09:46:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ dodger.style.bottom = "0px";
 function moveDodgerRight(){
     let leftNumbers = dodger.style.left.replace("px","");
     let left = parseInt(leftNumbers, 10);
-    if (left<game.offsetWidth - dodger.offsetWidth) {
-      dodger.style.left =`${left + 1}px`;
-      console.log(left);
+    if (left<360) {
+      dodger.style.left =`${left+1}px`;
     }
 }
+document.addEventListener("keydown", function(e){
+  if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+})
+
 function moveDodgerLeft(){
     let leftNumbers = dodger.style.left.replace("px","");
     let left = parseInt(leftNumbers, 10);
@@ -21,9 +26,6 @@ function moveDodgerLeft(){
     }
 }
 document.addEventListener("keydown", function(e){
-  if(e.key === "ArrowRight"){
-    moveDodgerRight();
-  }
   if(e.key === "ArrowLeft"){
     moveDodgerLeft();
   }
